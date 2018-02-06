a30ca94e6749872a2aa71b0484af125e8cc5fe87
Xavikh <xavikh@gmail.com>
2018-02-07 00:21:21 +0100

    Post request to API added Delay between shots added

--- programa.py
+++ programa.py
@@ -3,18 +3,31 @@ import RPi.GPIO as GPIO
 import time
 import sys
 import subprocess
+import requests
+
+idGun = 0
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.IN)
+GPIO.setup(26, GPIO.OUT)
+
 
 def evento_pulsador(channel):
-  print("creciente")
-  subprocess.Popen(['irsend','SEND_ONCE','/home/pi/lircd.conf','KEY_0'])
+    print("Shot")
+    subprocess.Popen(['irsend', 'SEND_ONCE', '/home/pi/lircd.conf', 'KEY_' + idGun])  # Try to send the message for 10ms
+    GPIO.output(26, GPIO.LOW)
+    time.sleep(300)
+    GPIO.output(26, GPIO.HIGH)
+
 
 GPIO.add_event_detect(17, GPIO.RISING)
 GPIO.add_event_callback(17, evento_pulsador)
 
-#time.sleep(1000)
 p = subprocess.Popen(["irw"], stdout=subprocess.PIPE)
 for line in p.stdout:
-  print(line.split()[2])
+    shot = line.split()[2]
+    print(shot)
+    idShot = shot[-1:]
+    payload = {'idGun': idShot, 'idVelt': 0}
+    r = requests.post("http://lasertag.coredumped.es/match/shot", data=payload)
+    print(r.text)
